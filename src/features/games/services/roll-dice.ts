a22147c0bc6119eac1cabc654e77fe50013a8430
Jaime <jmamani503@gmail.com>
2025-12-03 21:58:06 -0400

    fix: import errors roll dice service

--- src/features/games/services/roll-dice.ts
+++ src/features/games/services/roll-dice.ts
@@ -1,12 +1,5 @@
 import { api } from "../../../shared/api/api";
-import type { Dice } from "../models/dice.interface";
-import type { Game, Phase } from "../models/game.interface";
-
-interface Data {
-  gameId: string,
-  dice: Dice[],
-  phase: Phase
-}
+import type { Game } from "../models/game.interface";
 
 export const rollDice = async (roomId: string): Promise<Game> => {
   return await api.post('/game/roll', {roomId})
